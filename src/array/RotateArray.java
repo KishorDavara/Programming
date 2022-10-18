609813880d48555a769bfcfb365e0926453871a0
Kishor Davara <kishordavara.patel49@gmail.com>
2022-10-18 18:15:05 +0530

    array: rotate array elements in place

--- src/array/RotateArray.java
+++ src/array/RotateArray.java
@@ -13,7 +13,15 @@ public class RotateArray {
         System.out.println(Arrays.toString(nums));
     }
 
+
     public static void rotate(int[] nums, int k) {
+        //rotateUsingExtraSpace(nums,k);
+        rotateInPlace(nums,k);
+    }
+
+    //Time Complexity: O(n)
+    //Space Complexity: O(n)
+    private static void rotateUsingExtraSpace(int[] nums, int k) {
         int n = nums.length;
         k = k % n;
         int []ans = new int[n];
@@ -23,6 +31,30 @@ public class RotateArray {
         }
         for(int i = 0; i < n; i++) {
             nums[i] = ans[i];
+        }
+    }
+
+    private static void rotateInPlace(int [] nums, int k) {
+        // original array: [1,2,3,4,5,6,7]
+        k = k % nums.length;
+
+        // After below call: [7,6,5,4,3,2,1]
+        reverse(nums,0, nums.length - 1); // reverse entire array
+
+        // After below call: [5,6,7,4,3,2,1]
+        reverse(nums,0, k - 1); // reverse first k elements
+
+        // After below call: [5,6,7,1,2,3,4]
+        reverse(nums, k, nums.length - 1 ); // reverse remaining elements(k+1 to last elements)
+    }
+
+    private static void reverse(int [] nums, int start, int end) {
+        while(start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
     }
 }
