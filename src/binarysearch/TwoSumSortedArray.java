7ce3933202d6da2dcc648193eb38f46e2483ded0
Kishor Davara <kishordavara.patel49@gmail.com>
2022-11-06 21:32:08 +0530

    binary-search: two sum sorted array

--- src/binarysearch/TwoSumSortedArray.java
+++ src/binarysearch/TwoSumSortedArray.java
@@ -6,24 +6,50 @@ import java.util.Map;
 
 public class TwoSumSortedArray {
     public static void main(String[] args) {
-        int []numbers = {2,3,4};
-        System.out.println(Arrays.toString(twoSum(numbers,6)));
+        int[] numbers = {2, 3, 4};
+        System.out.println(Arrays.toString(twoSum(numbers, 6)));
+        System.out.println(Arrays.toString(twoSum_optimized(numbers,6)));
     }
 
-    public  static int[] twoSum(int[] numbers, int target) {
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int i = 0; i < numbers.length; i++) {
-            map.put(numbers[i],i + 1);
+    public static int[] twoSum(int[] numbers, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < numbers.length; i++) {
+            map.put(numbers[i], i + 1);
         }
-        int [] result = new int[2];
-        for(int i = 0; i < numbers.length; i++) {
-            int key = target-numbers[i];
-            if(map.containsKey(key)) {
+        int[] result = new int[2];
+        for (int i = 0; i < numbers.length; i++) {
+            int key = target - numbers[i];
+            if (map.containsKey(key)) {
                 result[0] = i + 1;
                 result[1] = map.get(key);
                 break;
             }
         }
         return result;
+    }
+
+    public static int[] twoSum_optimized(int[] nums, int target) {
+        for (int left = 0; left < nums.length; left++) {
+            int right = binarySearch(nums, left + 1, target - nums[left]);
+            if (right != -1) {
+                return new int[]{left + 1, right + 1};
+            }
+        }
+        return new int[]{};
+    }
+
+    private static int binarySearch(int[] nums, int left, int target) {
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (right + left) >> 1;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
     }
 }
